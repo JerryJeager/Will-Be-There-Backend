fad4741caf13aedb8a87c9ee475dc4baf221d1be
Amadi Jerry <amadijerry310@gmail.com>
2024-04-29 15:21:28 +0100

    add new email template

--- service/invitees/service.go
+++ service/invitees/service.go
@@ -82,7 +82,74 @@ func sendEmail(invitee *Invitee) error {
 	m.SetHeader("To", invitee.Email)
 	m.SetAddressHeader("Cc", invitee.Email, email)
 	m.SetHeader("Subject", "Will Be There")
-	m.SetBody("text/html", fmt.Sprintf("<h2>Hello %s </h2> <p>You're a guest for an event, %s</p>  <h3>Event Details</h3> <ul> <li>Venue: %s </li> <li>Date: %s </li> </ul> <i>See you there!</i>", invitee.FirstName, event.Name, event.Venue, event.Date ))
+	m.SetBody("text/html", fmt.Sprintf(`
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Will Be There</title>
+			<style>
+				body {{
+					font-family: Arial, sans-serif;
+					background-color: #f2f2f2;
+					padding: 20px;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					height: 100vh;
+					margin: 0;
+				}}
+				.email-container {{
+					max-width: 600px;
+					width: 100%%;
+					border-radius: 5px;
+					padding: 20px;
+					box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+				}}
+				h2 {{
+					color: #333333;
+					margin-bottom: 20px;
+				}}
+				p {{
+					color: #666666;
+					margin-bottom: 10px;
+				}}
+				ul {{
+					color: #666666;
+					margin-bottom: 10px;
+					padding-left: 20px;
+				}}
+				.footer-text {{
+					color: #666666;
+					margin-top: 20px;
+				}}
+			</style>
+		</head>
+		<body>
+			<div class="email-container">
+				<!-- Header -->
+				<table width="100%%" cellpadding="0" cellspacing="0" border="0">
+					<tr>
+						<td align="center">
+							<img src="%s" alt="Event Image" style="max-width: 200px;">
+						</td>
+					</tr>
+				</table>
+				<!-- Content -->
+				<h2>You're Invited!</h2>
+				<p>Dear %s,</p>
+				<p>You're invited to an event, %s</p>
+				<p>Event Details:</p>
+				<ul>
+					<li><strong>Date:</strong> %s</li>
+					<li><strong>Location:</strong> %s</li>
+				</ul>
+				<p>We look forward to seeing you at the event!</p>
+				<p class="footer-text">Best regards,<br>Will Be There Team.</p>
+			</div>
+		</body>
+	</html>
+	`, event.ImageUrl, invitee.FirstName, event.Name, event.Date, event.Venue))
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, emailUsername, emailPassword)
 
